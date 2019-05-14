fa576bcff865e0cbff5515a19923c5793a52a7d0
MaciejMilewski <36886297+MaciejMilewski@users.noreply.github.com>
2019-05-14 16:26:12 +0200

    Project files + Chromosome class + main

---
+++ GA_CS/GA_CS/Chromosome.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace GA_CS
+{
+    public class Chromosome
+    {
+        public Decimal Fitness { get; set; }
+        public Decimal[] Genes = new Decimal[2];
+
+        public Chromosome(Decimal gene_1, Decimal gene_2)
+        {
+            this.Genes[0] = gene_1;
+            this.Genes[1] = gene_2;
+        }
+
+        public void PrintChromosome()
+        {
+            Trace.Write("Genes: [");
+            Trace.Write(Genes[0].ToString());
+            Trace.Write(" ");
+            Trace.Write(Genes[1].ToString());
+            Trace.Write("]");
+            Trace.Write("\n");
+        }
+    }
+}
---
+++ GA_CS/GA_CS/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace GA_CS
+{
+    class Program
+    {
+        static void Main()
+        {
+            Trace.WriteLine("\n");
+
+            Chromosome ch1 = new Chromosome(2.0M, 2.4M);
+            ch1.PrintChromosome();
+
+            Trace.WriteLine("\n");
+        }
+    }
+}
