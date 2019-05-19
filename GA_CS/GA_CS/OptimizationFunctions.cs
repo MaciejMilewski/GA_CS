a53912c2a07b3d776902046e67f2c04ee4615ea7
MaciejMilewski <36886297+MaciejMilewski@users.noreply.github.com>
2019-05-19 21:15:19 +0200

    Added function that shows running time of code

--- GA_CS/GA_CS/OptimizationFunctions.cs
+++ GA_CS/GA_CS/OptimizationFunctions.cs
@@ -1,28 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class OptimizationFunctions
+namespace GA_CS
 {
-	public OptimizationFunctions() { }
-
-    public static double Beale(double x, double y)
+    public class OptimizationFunctions
     {
-        return (1.5 - x + x * y) * (1.5 - x + x * y) + (2.25 - x + x * y * y) * (2.25 - x + x * y * y) + (2.625 - x + x * y * y * y) * (2.625 - x + x * y * y * y);
-    }
+        public static double Beale(double x, double y)
+        {
+            return (1.5 - x + x * y) * (1.5 - x + x * y) + (2.25 - x + x * y * y) * (2.25 - x + x * y * y) + (2.625 - x + x * y * y * y) * (2.625 - x + x * y * y * y);
+        }
 
-    public static double Ackley(double x, double y)
-    {
-        return -20 * Math.Exp(-0.2 * Math.Sqrt(0.5 * (x * x + y * y))) -
-            Math.Exp(0.5 * (Math.Cos(2 * Math.PI * x) + Math.Cos(2 * Math.PI * y))) + Math.E + 20;
-    }
+        public static double Ackley(double x, double y)
+        {
+            return -20 * Math.Exp(-0.2 * Math.Sqrt(0.5 * (x * x + y * y))) -
+                Math.Exp(0.5 * (Math.Cos(2 * Math.PI * x) + Math.Cos(2 * Math.PI * y))) + Math.E + 20;
+        }
 
-    public static double Booth(double x, double y)
-    {
-        return (x + 2 * y - 7) * (x + 2 * y - 7) + (2 * x + y - 5) * (2 * x + y - 5);
-    }
+        public static double Booth(double x, double y)
+        {
+            return (x + 2 * y - 7) * (x + 2 * y - 7) + (2 * x + y - 5) * (2 * x + y - 5);
+        }
 
-    public static double HoedlerTable(double x, double y)
-    {
-        return -Math.Abs(Math.Sin(x) * Math.Cos(y) *
-            Math.Exp(Math.Abs(1 - Math.Sqrt(x * x + y * y) / Math.PI)));
+        public static double HoedlerTable(double x, double y)
+        {
+            return -Math.Abs(Math.Sin(x) * Math.Cos(y) *
+                Math.Exp(Math.Abs(1 - Math.Sqrt(x * x + y * y) / Math.PI)));
+        }
     }
 }
--- GA_CS/GA_CS/Program.cs
+++ GA_CS/GA_CS/Program.cs
@@ -9,24 +9,38 @@ namespace GA_CS
 {
     public class Program
     {
+        public static void PrintTime(DateTime t1, DateTime t2)
+        {
+            string time = "Time = ";
+            TimeSpan timeSpan = t2 - t1;
 
+            time += timeSpan.Minutes.ToString("D2") + ":";
+            time += timeSpan.Seconds.ToString("D2") + ".";
+            time += timeSpan.Milliseconds.ToString("D3");
+            time += " (min:sec:ms) " + "\r\n";
+
+            Trace.WriteLine(time);
+        }
         
-        static void Main()
+        public static void Main()
         {
             Trace.WriteLine("\n");
+            DateTime timeStart = DateTime.Now;
+            //
 
-            /*Chromosome ch1 = new Chromosome(2.0, 2.4);
+            Chromosome ch1 = new Chromosome(2.0, 2.4);
             ch1.PrintChromosome();
-            
-            f f1 = new f(Beale);
+
+            f f1 = new f(OptimizationFunctions.Beale);
             GeneticAlgorithm ga = new GeneticAlgorithm(10, 0.5, 0.4, 0.2, 50, f1, -5.0, 5.0);
             Trace.Write("Beale: ");
             Trace.Write(ga.fitnessFunction(3, 0.5).ToString());
             Trace.Write("\n");
-            Trace.WriteLine(ga.ToString());*/
-
-
-
+            Trace.WriteLine(ga.ToString());
+            
+            //
+            DateTime timeEnd = DateTime.Now;
+            PrintTime(timeStart, timeEnd);
             Trace.WriteLine("\n");
         }
     }
