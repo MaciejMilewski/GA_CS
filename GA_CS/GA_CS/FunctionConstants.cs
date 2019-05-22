0bb78ac90ea2a3b942226989ae8ca0806b4d1e48
Maciej Milewski <s17947@pjwstk.edu.pl>
2019-05-22 11:56:53 +0200

    Class file updated

---
+++ GA_CS/GA_CS/FunctionConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GA_CS
+{
+    public static class FunctionConstants
+    {
+        public static double[] ackleyLowerBound = { -5, -5 };
+        public static double[] ackleyUpperBound = { +5, +5 };
+        public static double ackleyBestFitness = 0.0;
+        public static double[] ackleyBestGenes = { 0.0, 0.0 };
+    }
+}
