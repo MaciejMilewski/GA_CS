c7bf918d1010afc5c6b74629ad085863fe20fcdb
MaciejMilewski <36886297+MaciejMilewski@users.noreply.github.com>
2019-05-24 21:57:56 +0200

    Added Particle Swarm class file

--- GA_CS/GA_CS/Program.cs
+++ GA_CS/GA_CS/Program.cs
@@ -33,15 +33,24 @@ namespace GA_CS
             DateTime timeStart = DateTime.Now;
 
             f f1 = new f(OptimizationFunctions.Ackley);
-            GeneticAlgorithm ga = new GeneticAlgorithm(6500, 2, 0.05, 0.04, 450, f1,FunctionConstants.ackleyLowerBound, FunctionConstants.ackleyUpperBound);
-            ga.GenerateInitialGenes();
-            ga.Initialize();
+            //GeneticAlgorithm ga = new GeneticAlgorithm(65000, 2, 0.05, 0.04, 450, f1,FunctionConstants.ackleyLowerBound, FunctionConstants.ackleyUpperBound);
+            //ga.GenerateInitialGenes();
+            //ga.Initialize();
+            HillClimbingAlgorithm hca = new HillClimbingAlgorithm(f1, 50000, FunctionConstants.ackleyLowerBound, FunctionConstants.ackleyUpperBound);
+            double[] start = new double[2];
+            start[0] = hca.RandomDouble(0);
+            start[1] = hca.RandomDouble(1);
+            Trace.Write("Start[0]: " + start[0].ToString() + "\r\n");
+            Trace.Write("Start[1]: " + start[1].ToString() + "\r\n");
+            //start[0] = 2.0;
+            //start[1] = -3.0;
+            hca.HillClimb(start);
 
             DateTime timeEnd = DateTime.Now;
             PrintTime(timeStart, timeEnd);
-            Trace.Write("Best Fitness: " + ga.BestFitness.ToString() + "\r\n");
-            Trace.Write("Best X: " + ga.BestGene[0] + "\r\n");
-            Trace.Write("Best Y: " + ga.BestGene[1] + "\r\n");
+            //Trace.Write("Best Fitness: " + ga.BestFitness.ToString() + "\r\n");
+            //Trace.Write("Best X: " + ga.BestGene[0] + "\r\n");
+            //Trace.Write("Best Y: " + ga.BestGene[1] + "\r\n");
             Trace.WriteLine("\n");
         }
     }
