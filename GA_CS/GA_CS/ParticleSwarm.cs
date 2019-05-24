9ecb1b59c5d9ad1c879603cdb0ce8ea3d059039d
MaciejMilewski <36886297+MaciejMilewski@users.noreply.github.com>
2019-05-24 22:14:29 +0200

    Added attributes and constructor in ParticleSwarm class

--- GA_CS/GA_CS/ParticleSwarm.cs
+++ GA_CS/GA_CS/ParticleSwarm.cs
@@ -8,6 +8,26 @@ namespace GA_CS
 {
     public class ParticleSwarm
     {
+        public int Size { get; set; }
+        public int Particles { get; set; }
+        public int Iterations { get; set; }
+        private int It { get; set; }
+        public double[] LowerLimit { get; set; }
+        public double[] UpperLimit { get; set; }
+        public f Function { get; set; }
+        public double BestResult { get; set; }
+        public double[] BestParameters { get; set; }
+
+        public ParticleSwarm(f function, int size, int particles, int iterations, double [] lowerlimit, double[] upperlimit)
+        {
+            this.Function = function;
+            this.Size = size;
+            this.Particles = particles;
+            this.Iterations = iterations;
+            this.LowerLimit = lowerlimit;
+            this.UpperLimit = upperlimit;
+            this.BestParameters = new double[Size];
+        }
 
     }
 }
